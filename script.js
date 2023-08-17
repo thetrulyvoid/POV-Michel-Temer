1f47adbd15923b6d0fb4ae23563b15711c19fcd9
Void <130793393+thetrulyvoid@users.noreply.github.com>
2023-08-17 12:08:36 -0300

    Files added by upload.
    The files of the program, the script in Javascript, the Index in HTML and the Style to HTML in CSS.

---
+++ script.js
@@ -0,0 +1,91 @@
+
+var cliques = 0;
+
+
+function Computador() {
+  cliques = cliques + 1;
+  
+  if(cliques == 1) {
+  var iniciandoMinecraft = "vtnc velho broxa"
+  var pegarVelhoBroxa = document.getElementById("textoDoComputador")
+  pegarVelhoBroxa.innerHTML = iniciandoMinecraft
+  var somIniciarMinecraft = document.getElementById("queDiaMaisExtressante").play()
+  var mudarBotaoIniciar = document.getElementById("Botão")
+  var mudarTextoBotaoIniciar = "Como é que é?"
+  mudarBotaoIniciar.innerHTML = mudarTextoBotaoIniciar
+ }
+  
+  
+  else if(cliques == 2){
+    var tmnmc = "iniciando app T.M.N.M.C"
+var pegarTMNMC = document.getElementById("textoDoComputador")
+  pegarTMNMC.innerHTML = tmnmc
+  var TMNMC = document.getElementById("TMNMC").play()
+  var mudarBotaoTMNMC = document.getElementById("Botão")
+  var mudarTextoBotaoTMNMC = "Mas o que é isso?"
+  mudarBotaoTMNMC.innerHTML = mudarTextoBotaoTMNMC
+  }
+  
+  else if(cliques == 3){
+    var tuaMaeNaMinhaCama = "Tua Mãe Na Minha Cama, é mole"
+var pegarTuaMaeNaMinhaCama = document.getElementById("textoDoComputador")
+  pegarTuaMaeNaMinhaCama.innerHTML = tuaMaeNaMinhaCama
+  var somTuaMaeNaMinhaCama = document.getElementById("TuaMaeNaMinhaCama").play()
+  var mudarBotaoTuaMaeNaMinhaCama = document.getElementById("Botão")
+  var mudarTextoBotaoTuaMaeNaMinhaCama = "Vai tomar no seu cu ô seu computador leproso, eu falei pra você iniciar Minecraft porra"
+  mudarBotaoTuaMaeNaMinhaCama.innerHTML = mudarTextoBotaoTuaMaeNaMinhaCama
+  }
+  
+  else if(cliques == 4){
+    var desinstalando = "Desinstalando todos os jogus"
+    var pegarDesinstalando = document.getElementById("textoDoComputador")
+  pegarDesinstalando.innerHTML = desinstalando
+  var somDesinstalando = document.getElementById("Desinstalando").play()
+  var mudarBotaoDesinstalando = document.getElementById("Botão")
+  var mudarTextoBotaoDesinstalando = "FILHO DA PUTAAAAAA"
+  mudarBotaoDesinstalando.innerHTML = mudarTextoBotaoDesinstalando
+  }
+  
+else if(cliques == 5){
+    var upload = "mae_do_usuario_nudes.rar [▂ ▃ ▅ ▆ ▇ ▉]"
+    var pegarUpload = document.getElementById("textoDoComputador")
+  pegarUpload.innerHTML = upload
+  var somUpload = document.getElementById("Upload").play()
+  var mudarBotaoUpload = document.getElementById("Botão")
+  var mudarTextoBotaoUpload = "NÃOOOOOOOOOOOO"
+  mudarBotaoUpload.innerHTML = mudarTextoBotaoUpload
+  }
+  
+else if(cliques == 6){
+    var emojiDeRato = "vsfporrakkkkkkkk 🔫🐀"
+    var pegarEmojiDeRato = document.getElementById("textoDoComputador")
+  pegarEmojiDeRato.innerHTML = emojiDeRato
+  var somEmojiDeRato = document.getElementById("EmojiDeRato").play()
+  var mudarBotaoEmojiDeRato = document.getElementById("Botão")
+  var mudarTextoBotaoEmojiDeRato = "Isso só pode ser vírus, quem instalou esse vírus aqui?"
+  mudarBotaoEmojiDeRato.innerHTML = mudarTextoBotaoEmojiDeRato
+  }
+  
+else if(cliques == 7){
+    var fuiEuPorra = "fui eu porra kkkkkkkk otário"
+    var pegarFuiEuPorra = document.getElementById("textoDoComputador")
+  pegarFuiEuPorra.innerHTML = fuiEuPorra
+  var somFuiEuPorra = document.getElementById("FuiEuPorra").play()
+  var mudarBotaoFuiEuPorra = document.getElementById("Botão")
+  var mudarTextoBotaoFuiEuPorra = "🤬"
+  mudarBotaoFuiEuPorra.innerHTML = mudarTextoBotaoFuiEuPorra
+  }
+  
+  else if(cliques == 8){
+    var autodestruicao = "iniciar autodestruição 💣"
+    var pegarAutodestruicao = document.getElementById("textoDoComputador")
+  pegarAutodestruicao.innerHTML = autodestruicao
+  var somAutodestruicao = document.getElementById("Autodestruicao").play()
+  var mudarBotaoAutodestruicao = document.getElementById("Botão")
+  var mudarTextoBotaoAutodestruicao = "AHHHHHHHHHHH"
+  mudarBotaoAutodestruicao.innerHTML = mudarTextoBotaoAutodestruicao
+  }
+}
+
+
+
